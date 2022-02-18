3092f6812a4a868b65c304fd244334e642ee3c68
Ernest Cheung <ernestcheung02@gmail.com>
2022-02-17 18:40:41 -0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
+import Tweet from './tweet.js';
 import './App.css';
+import Recipe from './Recipe';
+import Selection from './Selection.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [counter, setCounter] = useState([0, 0, 0]);
+	const [users, setUsers] = useState([
+		{ name: 'e', message: 'hello' },
+		{ name: 'd', message: 'hello' },
+		{ name: 'f', message: 'hello' },
+	]);
+	useEffect(() => {
+		console.log('run');
+	}, [counter]);
+
+	const [search, setSearch] = useState('');
+	const [query, setQuery] = useState('');
+
+	const getRecipes = async () => {
+		const response = await fetch();
+		const data = await response.json();
+		console.log(data);
+	};
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+	};
+	const getSearch = (e) => {
+		e.preventDefault();
+		setQuery(search);
+	};
+
+	const [total, SetTotal] = useState(0);
+	const getTotal = () => {};
+
+	console.log(counter);
+
+	return (
+		<div className="app">
+			<form className="searh form" onSubmit={getSearch}>
+				<input
+					className="search-bar"
+					type="text"
+					value={search}
+					onChange={updateSearch}
+				/>
+				<button className="search-button" type="submit">
+					search
+				</button>
+			</form>
+			<h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+			{counter.map((val, i) => (
+				<Selection
+					value={val}
+					increment={() => {
+						const newCount = [...counter];
+						newCount[i] = val + 1;
+						setCounter(newCount);
+					}}
+				/>
+			))}
+			<button
+				onClick={() => {
+					let temp = 0;
+					counter.forEach((each) => {
+						temp += each;
+						SetTotal(temp);
+					});
+				}}
+			>
+				{' '}
+				Submit
+			</button>
+
+			{total}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Selection.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
+function Selection(props) {
+	const { price, name, value, increment } = props;
+
+	return (
+		<div className="Selection">
+			<h1 className="space">{name}</h1>
+			<h1 className="space">${price}</h1>
+			<button onClick={() => props.increment()} className="space">
+				+
+			</button>
+			<h1>{value}</h1>
+		</div>
+	);
+}
+
+export default Selection;
